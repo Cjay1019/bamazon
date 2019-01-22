b3e94a72e5021cf65241f918dd13d13a534a71c6
Cjay1019 <cjay741@gmail.com>
2019-01-22 16:39:10 -0500

    added requirements and created a printInventory function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,11 +13,34 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("Welcome to Bamazon!\n");
   printInventory();
 });
 
-function printInventory() {}
+function printInventory() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    table.push(["Product".blue, "Department".blue, "Price".blue, "Stock".blue]);
+    for (i = 0; i < res.length; i++) {
+      var product = [];
+      product.push(
+        res[i].product_name,
+        res[i].department_name,
+        "$" + res[i].price,
+        res[i].stock_quantity
+      );
+      table.push(product);
+    }
+    console.log("" + table);
+  });
+}
 
-// table.push(["First".blue, "Second"], ["Foo", "Bar"]);
-
-// console.log("" + table);
+function buy() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "choice",
+      message: "Which item would you like to buy?"
+    }
+  ]);
+}
